2c3ef405eebde12472722b6063800b8fdd733ff2
Toni <smokercho56@gmail.com>
2024-02-16 21:57:05 +0200

    Created the measures model for handling the height, weight and activity of the user

--- server/health/models.py
+++ server/health/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from server.profiles.models import Profile
+
+from enum import Enum
+
+from server.mixins import ChoicesEnumMixin
+
+
+class ActivityChoicesMixin(ChoicesEnumMixin, Enum):
+    Sedentary = "Sedentary"
+    Light = "Light"
+    Moderate = "Moderate"
+    Very = "Very"
+    Extreme = "Extreme"
+
+
+class Measures(models.Model):
+    height = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+    )
+    weight = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+    )
+    activity = models.CharField(
+        choices=ActivityChoicesMixin.choices(),
+        max_length=ActivityChoicesMixin.max_len(),
+    )
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
